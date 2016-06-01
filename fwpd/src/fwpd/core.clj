565ec385a5df9939bba53c08791b9f247f4f617d
Scott <eiselesr@gmail.com>
2016-06-01 10:52:36 -0500

    Finished chapter 4

--- fwpd/src/fwpd/core.clj
+++ fwpd/src/fwpd/core.clj
@@ -82,8 +82,9 @@
 (first (mapify (parse (slurp filename))))
 ; => {:name "Edward Cullen", :glitter-index 10}
 
-(mapify vampStringAllSplit)
-
+(def vampMap
+  (mapify vampStringAllSplit))
+(identity vampMap)
 
 (defn glitter-filter
   [minimum-glitter records]
@@ -100,3 +101,53 @@
   (map :name (filter #(>= (:glitter-index %) minimum-glitter) records)))
 
 (glitter-filter2 3 (mapify (parse (slurp filename))))
+
+;Write a function, validate, which will check that :name and :glitter-index are ;present when you append. The validate function should accept two arguments: a ;map of keywords to validating functions, similar to conversions, and the record ;to be validated.
+
+(conj (mapify vampStringAllSplit) {:name "Todd" :glitter-index 7})
+
+(defn append
+  [suspectList newSuspect]
+  (if (validate newSuspect)
+    (conj suspectList newSuspect)
+    ;(identity suspectList)))
+    false))
+
+
+(defn validate
+  [newSuspect]
+  (and (:name newSuspect) (:glitter-index newSuspect)))
+
+;Write a function that will take your list of maps and convert it back to a CSV string. You’ll need to use the clojure.string/join function.
+
+;"Edward Cullen,10\nBella Swan,0\nCharlie Swan,0\nJacob Black,3\nCarlisle Cullen,6\n"
+
+;({:name "Edward Cullen", :glitter-index 10}
+; {:name "Bella Swan", :glitter-index 0}
+; {:name "Charlie Swan", :glitter-index 0}
+; {:name "Jacob Black", :glitter-index 3}
+; {:name "Carlisle Cullen", :glitter-index 6})
+
+(clojure.string/join "," (map vals vampMap))
+
+(vals (first vampMap))
+
+(str (:name (first vampMap)) "," (:glitter-index (first vampMap)))
+
+(#(str (:name %) "," (:glitter-index %)) (first vampMap))
+
+(map #(str (:name %) "," (:glitter-index %) "\n") vampMap)
+
+(clojure.string/join (map #(str (:name %) "," (:glitter-index %) "\n") vampMap))
+
+(defn map2csv
+  [vampMaps]
+  (clojure.string/join (map #(str (:name %) "," (:glitter-index %) "\n") vampMap)))
+
+;-----------Test functions--------------------------------------------
+(append (mapify vampStringAllSplit) {:names "Todd" :glitter-index 7})
+(validate {:name "Todd" :glitter-index 7})
+(validate {:name "Todd"})
+(validate {})
+
+(map2csv vampMap)
