005c38462a31e7f5149cdcf6c18e2f7dd03b8b2d
Scott <eiselesr@gmail.com>
2016-06-02 18:03:46 -0500

    Got through memoize section

--- src/Functional_Programming/core.clj
+++ src/Functional_Programming/core.clj
@@ -25,8 +25,65 @@
 (apply add-inc '(1 2 3 4 5))
 
 (defn myComp
-  [& fns]
-  (fn [& args]
-    ()))
+  [f & fns]
+  (if (empty? fns)
+    f
+    (recur (fn [& args]
+            (f (apply (first fns) args)))
+           (rest fns))))
 
-(myComp inc + -)
+(def myCompfunc
+  (myComp inc inc inc))
+
+(myCompfunc 1)
+
+(def testcomp
+  (comp inc + -))
+
+(testcomp 1)
+
+;--------------------------------------------------
+;memoize
+;; Fibonacci number with recursion.
+(defn fib [n]
+  (condp = n
+    0 1
+    1 1
+    (+ (fib (dec n)) (fib (- n 2)))))
+
+(time (fib 30))
+;; "Elapsed time: 8179.04028 msecs"
+
+;; Fibonacci number with recursion and memoize.
+(def m-fib
+  (memoize (fn [n]
+             (condp = n
+               0 1
+               1 1
+               (+ (m-fib (dec n)) (m-fib (- n 2)))))))
+
+(time (m-fib 90))
+;; "Elapsed time: 1.282557 msecs"
+
+
+(defn sleepy-identity
+  "Returns the given value after 1 second"
+  [x]
+  (Thread/sleep 1000)
+  x)
+(time
+  (sleepy-identity "Mr. Fantastico"))
+; => "Mr. Fantastico" after 1 second
+
+(time
+  (sleepy-identity "Mr. Fantastico"))
+; => "Mr. Fantastico" after 1 second
+
+(def memo-sleepy-identity (memoize sleepy-identity))
+(time
+  (memo-sleepy-identity "Mr. Fantastico"))
+; => "Mr. Fantastico" after 1 second
+
+(time
+  (memo-sleepy-identity "Mr. Fantastico"))
+; => "Mr. Fantastico" immediately
